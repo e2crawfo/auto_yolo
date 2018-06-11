9bb4a2f0214c32c0ecf193e40708e217488684bf
Eric Crawford <eric.crawford@mail.mcgill.ca>
2018-06-11 14:32:52 -0700

    Config twiddle.

--- experiments/core/yolo_air_transfer.py
+++ experiments/core/yolo_air_transfer.py
@@ -2,22 +2,32 @@ from auto_yolo import envs
 
 readme = "redoing yolo_air transfer experiment"
 
+distributions = [
+    dict(min_chars=1, max_chars=5),
+    dict(min_chars=6, max_chars=10),
+    dict(min_chars=11, max_chars=15),
+]
+
 durations = dict(
     long=dict(
-        max_hosts=1, ppn=4, cpp=2, gpu_set="0,1", wall_time="24hours",
+        max_hosts=1, ppn=6, cpp=2, gpu_set="0,1", wall_time="24hours",
         project="rpp-bengioy", cleanup_time="20mins",
-        slack_time="5mins", n_repeats=8, step_time_limit="24hours"),
+        slack_time="5mins", n_repeats=6, step_time_limit="24hours"),
+
     build=dict(
-        max_hosts=1, ppn=1, cpp=2, gpu_set="0", wall_time="2hours",
+        max_hosts=1, ppn=3, cpp=2, gpu_set="0", wall_time="2hours",
         project="rpp-bengioy", cleanup_time="2mins",
-        slack_time="2mins", n_repeats=1, step_time_limit="2hours"),
+        slack_time="2mins", n_repeats=1, step_time_limit="2hours",
+        config=dict(do_train=False)),
+
     short=dict(
-        max_hosts=1, ppn=1, cpp=2, gpu_set="0", wall_time="4hours",
-        project="rpp-bengioy", cleanup_time="2mins",
-        slack_time="2mins", n_repeats=1, step_time_limit="4hours"),
+        max_hosts=1, ppn=2, cpp=2, gpu_set="0", wall_time="20mins",
+        project="rpp-bengioy", cleanup_time="1mins",
+        slack_time="1mins", n_repeats=1, n_param_settings=4),
 )
 
 envs.run_experiment(
     "yolo_air_transfer", dict(), readme,
     alg="yolo_air_transfer", task="scatter", durations=durations,
+    distributions=distributions
 )
