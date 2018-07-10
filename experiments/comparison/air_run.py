c95042ec05671b4a2edcdf63330e7c85bd9ac966
Eric Crawford <eric.crawford@mail.mcgill.ca>
2018-07-10 09:27:58 -0700

    Moved some of core experiments to comparison.

---
+++ experiments/comparison/air_run.py
@@ -0,0 +1,74 @@
+from auto_yolo import envs
+
+readme = "Running AIR experiment."
+
+distributions = [
+    {'n_digits': 1,
+     'rnn_n_units': 128,
+     'scale_prior_mean': -2.9444389791664403,
+     'scale_prior_std': 0.2,
+     'shift_prior_std': 1.0},
+
+    {'n_digits': 3,
+     'rnn_n_units': 64,
+     'scale_prior_mean': -2.197224577336219,
+     'scale_prior_std': 0.2,
+     'shift_prior_std': 1.0},
+
+    {'n_digits': 5,
+     'rnn_n_units': 64,
+     'scale_prior_mean': -2.197224577336219,
+     'scale_prior_std': 0.2,
+     'shift_prior_std': 2.0},
+
+    {'n_digits': 7,
+     'rnn_n_units': 128,
+     'scale_prior_mean': -1.3862943611198906,
+     'scale_prior_std': 0.4,
+     'shift_prior_std': 4.0},
+
+    {'n_digits': 9,
+     'rnn_n_units': 128,
+     'scale_prior_mean': -0.4054651081081643,
+     'scale_prior_std': 0.2,
+     'shift_prior_std': 1.0}
+]
+
+
+for dist in distributions:
+    n_digits = dist['n_digits']
+    dist.update(
+        min_digits=n_digits,
+        max_digits=n_digits,
+        max_time_steps=n_digits)
+
+
+durations = dict(
+    long=dict(
+        max_hosts=1, ppn=16, cpp=1, gpu_set="0,1,2,3", wall_time="24hours",
+        project="rpp-bengioy", cleanup_time="10mins",
+        slack_time="10mins", n_repeats=6, step_time_limit="24hours"),
+
+    build=dict(
+        max_hosts=1, ppn=1, cpp=2, gpu_set="0", wall_time="20mins",
+        project="rpp-bengioy", cleanup_time="2mins",
+        slack_time="2mins", n_repeats=1, step_time_limit="2hours",
+        config=dict(do_train=False), n_param_settings=1,),
+
+    short=dict(
+        max_hosts=1, ppn=2, cpp=2, gpu_set="0", wall_time="20mins",
+        project="rpp-bengioy", cleanup_time="1mins",
+        slack_time="1mins", n_repeats=1, n_param_settings=4),
+)
+
+config = dict(
+    curriculum=[dict()],
+    n_train=64000, run_all_time_steps=True,
+    stopping_criteria="AP,max", threshold=0.99, patience=50000,
+)
+
+
+envs.run_experiment(
+    "air_run", config, readme, distributions=distributions,
+    alg="air", task="arithmetic", durations=durations,
+)
---
+++ experiments/comparison/dair_search.py
@@ -0,0 +1,68 @@
+import numpy as np
+import argparse
+
+from auto_yolo import envs
+
+readme = "searching for best hyperparameters for varying numbers of digits"
+
+p = np.array([0.05, 0.1, 0.2, 0.4])
+distributions = dict(
+    scale_prior_mean=np.log(p/(1-p)),
+    scale_prior_std=[.2, .4, .6, .8, 1.0],
+    shift_prior_std=[0.5, 1.0, 2.0, 4.0],
+    rnn_n_units=[64, 128, 256],
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n-digits", type=int, default=1)
+args, _ = parser.parse_known_args()
+
+
+durations = dict(
+    long=dict(
+        max_hosts=1, ppn=12, cpp=2, gpu_set="0,1,2,3", wall_time="24hours",
+        project="rpp-bengioy", cleanup_time="5mins",
+        slack_time="5mins", n_repeats=1, step_time_limit="24hours"),
+
+    test=dict(
+        max_hosts=1, ppn=12, cpp=2, gpu_set="0,1,2,3", wall_time="30mins",
+        project="rpp-bengioy", cleanup_time="2mins",
+        slack_time="2mins", n_repeats=1, config=dict(do_train=False), n_param_settings=24),
+
+    build=dict(
+        max_hosts=1, ppn=1, cpp=2, gpu_set="0", wall_time="20mins",
+        project="rpp-bengioy", cleanup_time="2mins",
+        slack_time="2mins", n_repeats=1, step_time_limit="2hours",
+        config=dict(do_train=False), n_param_settings=1,),
+
+    short=dict(
+        max_hosts=1, ppn=2, cpp=2, gpu_set="0", wall_time="20mins",
+        project="rpp-bengioy", cleanup_time="1mins",
+        slack_time="1mins", n_repeats=1, n_param_settings=4),
+
+    small_oak=dict(
+        max_hosts=1, ppn=2, cpp=2, gpu_set="0", wall_time="30mins",
+        cleanup_time="1mins", n_param_settings=4,
+        slack_time="1mins", n_repeats=1, kind="parallel", host_pool=":"),
+
+    build_oak=dict(
+        max_hosts=1, ppn=1, cpp=2, gpu_set="0", wall_time="30mins",
+        cleanup_time="1mins", n_param_settings=1,
+        slack_time="1mins", n_repeats=1, kind="parallel", host_pool=":",
+        config=dict(do_train=False)),
+)
+
+n_digits = args.n_digits
+
+config = dict(
+    curriculum=[dict()],
+    n_train=64000, min_digits=n_digits, max_digits=n_digits,
+    max_time_steps=n_digits, run_all_time_steps=True,
+    stopping_criteria="AP,max", threshold=0.99, patience=10000,
+)
+
+
+envs.run_experiment(
+    "dair_search_n_digits={}".format(n_digits), config, readme,
+    distributions=distributions, alg="dair", task="arithmetic", durations=durations,
+)
