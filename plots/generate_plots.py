bb0ea0ad2223b1d23325634d5f23d78b1b66da09
Eric Crawford <eric.crawford@mail.mcgill.ca>
2018-07-30 15:29:50 -0400

    Redo addition plots.

--- plots/generate_plots.py
+++ plots/generate_plots.py
@@ -513,27 +513,74 @@ def plot_comparison(extension):
 
 
 def plot_addition(extension):
-    baseline_path = os.path.join(data_dir, "addition/run_search_addition_env=size=14-in-colour=False-task=arithmetic2_alg=baseline-math_duration=long_seed=0_2018_07_22_14_55_04")
-    ground_truth_path = os.path.join(data_dir, "addition/run_search_addition_env=size=14-in-colour=False-task=arithmetic2_alg=ground-truth-math_duration=long_seed=0_2018_07_22_14_56_39")
-    simple_path = os.path.join(data_dir, "addition/run_search_addition_env=size=14-in-colour=False-task=arithmetic2_alg=simple-math_duration=long_seed=0_2018_07_22_14_57_30")
-    # yolo_air_path = os.path.join(data_dir, )
+    air_fixed_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=fixed_alg=air-math_duration=long_seed=0_2018_07_29_09_58_58/")
+    baseline_fixed_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=fixed_alg=baseline-math_duration=long_seed=0_2018_07_28_22_01_21/")
+    ground_truth_fixed_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=fixed_alg=ground-truth-math_duration=long_seed=0_2018_07_28_22_02_14/")
+    simple_fixed_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=fixed_alg=simple-math_duration=long_seed=0_2018_07_28_22_03_18/")
+    yolo_air_fixed_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=fixed_alg=yolo-air-math_duration=long_seed=0_2018_07_28_22_04_04/")
+
+    baseline_raw_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=raw_alg=baseline-math_duration=long_seed=0_2018_07_28_22_00_58/")
+    ground_truth_raw_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=raw_alg=ground-truth-math_duration=long_seed=0_2018_07_28_22_01_56/")
+    simple_raw_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=raw_alg=simple-math_duration=long_seed=0_2018_07_28_22_02_59/")
+
+    air_unfixed_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=unfixed_alg=air-math_duration=long_seed=0_2018_07_29_09_58_32/")
+    baseline_unfixed_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=unfixed_alg=baseline-math_duration=long_seed=0_2018_07_28_22_01_39/")
+    ground_truth_unfixed_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=unfixed_alg=ground-truth-math_duration=long_seed=0_2018_07_28_22_02_30/")
+    simple_unfixed_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=unfixed_alg=simple-math_duration=long_seed=0_2018_07_28_22_03_41/")
+    yolo_air_unfixed_path = os.path.join(data_dir, "addition/stage2/run_search_addition-stage2_env=task=arithmetic2_run-kind=unfixed_alg=yolo-air-math_duration=long_seed=0_2018_07_28_22_04_21/")
 
     # -----
 
     fig = plt.figure(figsize=(5, 3.5))
     ax = plt.gca()
 
-    x, y, *yerr = get_arithmetic_data([baseline_path], "n_train", "_test_math_accuracy", 0, "ci95")
-    line = ax.errorbar(x, y, yerr=yerr, label="Baseline", marker="o", ls="-")
-    line.lines[0].get_c()
+    x, y, *yerr = get_arithmetic_data([air_fixed_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="AIR - Fixed", marker="o", ls="--")
+    air_color = line.lines[0].get_c()
 
-    x, y, *yerr = get_arithmetic_data([ground_truth_path], "n_train", "_test_math_accuracy", 0, "ci95")
-    line = ax.errorbar(x, y, yerr=yerr, label="Ground Truth", marker="^", ls="-.")
-    line.lines[0].get_c()
+    x, y, *yerr = get_arithmetic_data([baseline_fixed_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="Baseline - Fixed", marker="o", ls="--")
+    baseline_color = line.lines[0].get_c()
 
-    x, y, *yerr = get_arithmetic_data([simple_path], "n_train", "_test_math_accuracy", 0, "ci95")
-    line = ax.errorbar(x, y, yerr=yerr, label="CNN", marker="v", ls="--")
-    line.lines[0].get_c()
+    x, y, *yerr = get_arithmetic_data([ground_truth_fixed_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="Ground Truth - Fixed", marker="o", ls="--")
+    ground_truth_color = line.lines[0].get_c()
+
+    x, y, *yerr = get_arithmetic_data([simple_fixed_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="CNN - Fixed", marker="^", ls="--")
+    simple_color = line.lines[0].get_c()
+
+    x, y, *yerr = get_arithmetic_data([yolo_air_fixed_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="SPAIR - Fixed", marker="v", ls="--")
+    yolo_air_color = line.lines[0].get_c()
+
+    # -----
+
+    x, y, *yerr = get_arithmetic_data([baseline_raw_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="Baseline", marker="o", ls="-", color=baseline_color)
+
+    x, y, *yerr = get_arithmetic_data([ground_truth_raw_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="Ground Truth", marker="^", ls="-", color=ground_truth_color)
+
+    x, y, *yerr = get_arithmetic_data([simple_raw_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="CNN", marker="v", ls="-", color=simple_color)
+
+    # -----
+
+    x, y, *yerr = get_arithmetic_data([air_unfixed_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="AIR - Unfixed", marker="o", ls="-.", color=air_color)
+
+    x, y, *yerr = get_arithmetic_data([baseline_unfixed_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="Baseline - Unfixed", marker="o", ls="-.", color=baseline_color)
+
+    x, y, *yerr = get_arithmetic_data([ground_truth_unfixed_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="Ground Truth - Unfixed", marker="o", ls="-.", color=ground_truth_color)
+
+    x, y, *yerr = get_arithmetic_data([simple_unfixed_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="CNN - Unfixed", marker="^", ls="-.", color=simple_color)
+
+    x, y, *yerr = get_arithmetic_data([yolo_air_unfixed_path], "n_train", "_test_math_accuracy", 0, "ci95")
+    line = ax.errorbar(x, y, yerr=yerr, label="SPAIR - Unfixed", marker="v", ls="-.", color=yolo_air_color)
 
     ax.set_ylabel('Accuracy', fontsize=12)
     ax.set_xlabel('\# Training Examples / 1000', fontsize=12)
@@ -543,7 +590,7 @@ def plot_addition(extension):
     ax.set_xticks(x)
     ax.set_xticklabels((np.array(x) / 1000).astype('i'))
 
-    plt.legend(loc="upper left", handlelength=4)
+    # plt.legend(loc="upper left", handlelength=4)
     plt.subplots_adjust(left=0.12, bottom=0.13, right=0.99, top=0.99)
     plot_path = os.path.join(plot_dir, 'addition/main.' + extension)
     os.makedirs(os.path.dirname(plot_path), exist_ok=True)
