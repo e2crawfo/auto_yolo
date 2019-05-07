Fixed minor bug in AP calculation.

auto_yolo/models/core.py
@@ -180,8 +180,8 @@ def mAP(pred_boxes, gt_boxes, n_classes, recall_values=None, iou_threshold=None)
     for c in range(n_classes):
         _ap = []
         for iou_thresh in iou_threshold:
-            predicted_list = []  # Each element is (confidence, ground-truth (0 or 1))
-            n_positives = 0
+            predicted_list = []  # Each element is of the form (confidence, ground-truth (0 or 1))
+            n_positives_gt = 0
 
             for pred, gt in zip(pred_boxes, gt_boxes):
                 # Within a single image
@@ -192,28 +192,28 @@ def mAP(pred_boxes, gt_boxes, n_classes, recall_values=None, iou_threshold=None)
                 pred_c = [(*b, a) for b, a in zip(pred_c, area)]
 
                 gt_c = [b for cls, *b in gt if cls == c]
-                n_positives += len(gt_c)
+                n_positives_gt += len(gt_c)
 
                 if not gt_c:
-                    predicted_list.extend((conf, 0) for conf, *b in pred_c)
+                    predicted_list.extend((conf, 0) for conf, *_ in pred_c)
                     continue
 
                 gt_c = np.array(gt_c)
                 gt_c_area = (gt_c[:, 1] - gt_c[:, 0]) * (gt_c[:, 3] - gt_c[:, 2])
                 gt_c = np.concatenate([gt_c, gt_c_area[..., None]], axis=1)
 
+                used = [0] * len(gt_c)
+
                 for conf, *box in pred_c:
                     iou = compute_iou(box, gt_c)
                     best_idx = np.argmax(iou)
                     best_iou = iou[best_idx]
-                    if best_iou > iou_thresh:
+
+                    if best_iou > iou_thresh and not used[best_idx]:
                         predicted_list.append((conf, 1.))
-                        gt_c = np.delete(gt_c, best_idx, axis=0)
+                        used[best_idx] = 1
                     else:
                         predicted_list.append((conf, 0.))
-
-                    if not gt_c.shape[0]:
-                        break
 
             if not predicted_list:
                 ap.append(0.0)
@@ -225,11 +225,12 @@ def mAP(pred_boxes, gt_boxes, n_classes, recall_values=None, iou_threshold=None)
             # Compute AP
             cs = np.cumsum(predicted_list[:, 1])
             precision = cs / (np.arange(predicted_list.shape[0]) + 1)
-            recall = cs / n_positives
+            recall = cs / n_positives_gt
 
             for r in recall_values:
                 p = precision[recall >= r]
                 _ap.append(0. if p.size == 0 else p.max())
+
         ap.append(np.mean(_ap))
     return np.mean(ap)
 