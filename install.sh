ad86737055d9b64f9e098ea65623334e21e613c0
Eric Crawford <eric.crawford@mail.mcgill.ca>
2020-06-16 10:41:18 -0400

    Added disclaimer about tf version, and code to check tf version when installing.

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+# Check tensorflow version.
+
+MAX_VERSION=1.13
+VALID_TF_VERSION=$(python -c "import tensorflow as tf; from pkg_resources import parse_version; print(parse_version(tf.VERSION) < parse_version('$MAX_VERSION'))")
+
+if [ $VALID_TF_VERSION != "True" ]; then
+  TF_VERSION=$(python -c "import tensorflow as tf; print(tf.VERSION)")
+  echo "Version $TF_VERSION too new, must be < $MAX_VERSION"
+fi
+
 GIT_BRANCH=$(git branch | sed -n '/\* /s///p')
 echo "*** git branch for auto_yolo is <"$GIT_BRANCH">."
 
@@ -11,9 +21,11 @@ pip install -r requirements.txt
 pip install -e .
 
 # Install auto_yolo
-cd  ../
+cd  ..
 pip install -r requirements.txt
 pip install -e .
 cd auto_yolo/tf_ops/render_sprites/
 make
-cd ../../../
+cd ../resampler_edge
+make
+cd ../../..
